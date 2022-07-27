4ff7023c6340ec53ac50b2bd0c14e79befa667dc
Jun Allard <jun.allard@uci.edu>
2022-07-26 20:46:32 -0700

    four keyword finders

---
+++ keywordExtractor3.py
@@ -0,0 +1,27 @@
+# script.py
+import pke
+
+# initialize keyphrase extraction model, here TopicRank
+extractor = pke.unsupervised.TopicRank()
+
+# load the content of the document, here document is expected to be in raw
+# format (i.e. a simple text file) and preprocessing is carried out using spacy
+extractor.load_document(input='summary.txt', language='en')
+
+# keyphrase candidate selection, in the case of TopicRank: sequences of nouns
+# and adjectives (i.e. `(Noun|Adj)*`)
+extractor.candidate_selection()
+
+# candidate weighting, in the case of TopicRank: using a random walk algorithm
+extractor.candidate_weighting()
+
+# N-best selection, keyphrases contains the 10 highest scored candidates as
+# (keyphrase, score) tuples
+keyphrases = extractor.get_n_best(n=10)
+
+
+# print the n-highest (10) scored candidates
+for (keyphrase, score) in extractor.get_n_best(n=10, stemming=True):
+    print(keyphrase, score)
+
+print(len(keyphrases))
