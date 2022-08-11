1a33c5e5cb61adac1e122ce18022971e6b6910bf
Jun Allard <jun.allard@uci.edu>
2022-08-10 23:27:56 -0700

    vignettes with photos first 5 samples

--- alpha_figManipulate.py
+++ alpha_figManipulate.py
@@ -1,41 +1,86 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from PIL import PSDraw
 
-image_folder = 'data/photos/'
+import math
 
-im = Image.open(image_folder + "ShiX.jpeg")
+from PIWordCloud import PI
 
-out = im.resize((128, 128))
+def makeVignette(thisPI):
 
-out.show()
+    title_short_side = 200
+
+    photo_folder = 'data/photos/'
+
+    photo_raw = Image.open(photo_folder + thisPI.lastName + thisPI.firstName[0] + ".jpeg")
+
+    if photo_raw.width < photo_raw.height:
+        vignette_is_landscape = 1
+    else:
+        vignette_is_landscape = 0
+
+    img_wordcloud = Image.open('results/220807/' + thisPI.lastName + '.png')
+
+    if vignette_is_landscape:
+        vignette_height = img_wordcloud.height
+        photo_height = math.ceil(vignette_height)
+        photo_width = math.ceil(photo_height/photo_raw.height*photo_raw.width)
+        title_long_side = img_wordcloud.height
+        vignette_width = img_wordcloud.width+photo_width+title_short_side
+
+    else:
+        vignette_width = img_wordcloud.width
+        photo_width = math.ceil(vignette_width)
+        photo_height = math.ceil(photo_width/photo_raw.width*photo_raw.height)
+        title_long_side = img_wordcloud.width
+        vignette_height = img_wordcloud.height+photo_height+title_short_side    
+
+    # ------ Resize
+   
+    photo = photo_raw.resize((photo_width, photo_height))
+
+    # ------- Create text
+
+    title = thisPI.firstName.upper() + " " + thisPI.lastName.upper()
+
+    img_title = Image.new("RGBA", (title_long_side,title_short_side), color=(255,255,255))
+    # Image is converted into editable form using
+    # Draw function and assigned to d1
+    img_draw = ImageDraw.Draw(img_title)
+    # Font selection and size selection
+    myFont = ImageFont.truetype("Arial.ttf", 200)
+    title_size = myFont.getsize(title)
+    myFont = ImageFont.truetype("Arial.ttf", math.floor(title_long_side*200/title_size[0]))
 
 
-if 0:
+    # Decide the text location, color and font
+    #d1.text((65, 10), "Sample text", fill =(255, 0, 0),font=myFont)
+    img_draw.text((title_long_side/2, 0), title, align="center",anchor='ma', fill =(0, 0, 0),font=myFont)
+
+    if vignette_is_landscape:
+        img_title = img_title.rotate(angle=90,expand=True)
+
+    # show and save the image
+    #img.show()
+    #img_title.save("title.png")
 
 
-    with Image.open("hopper.ppm") as im:
-        title = "hopper"
-        box = (1 * 72, 2 * 72, 7 * 72, 10 * 72)  # in points
+    # -------  Merge
+    image_vignette = Image.new("RGBA", (vignette_width,vignette_height))
+    image_vignette.paste(img_wordcloud)
 
-        ps = PSDraw.PSDraw()  # default is sys.stdout or sys.stdout.buffer
-        ps.begin_document(title)
+    if vignette_is_landscape:
 
-        # draw the image (75 dpi)
-        ps.image(box, im, 75)
-        ps.rectangle(box)
+        image_vignette.paste(photo, (img_wordcloud.width, 0))
+        image_vignette.paste(img_title,(img_wordcloud.width+photo_width,0))
 
-        # draw title
-        ps.setfont("HelveticaNarrow-Bold", 36)
-        ps.text((3 * 72, 4 * 72), title)
+    else:
+        image_vignette.paste(photo, (0, img_wordcloud.height))
+        image_vignette.paste(img_title,(0,img_wordcloud.height+photo_height))
 
-        ps.end_document()
+    image_vignette.save("results/vignettes/" + thisPI.lastName + ".png")
 
-    def merge(im1, im2):
-        w = im1.size[0] + im2.size[0]
-        h = max(im1.size[1], im2.size[1])
-        im = Image.new("RGBA", (w, h))
 
-        im.paste(im1)
-        im.paste(im2, (im1.size[0], 0))
+if __name__ == "__main__":
 
-        return im
+    thisPI = PI("Rodriguez-Verdugo", "Alejandra")
+    makeVignette(thisPI)
