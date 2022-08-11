df1d1968b99c3ce8400a866c3da9b5b4cc353d4e
Jun Allard <jun.allard@uci.edu>
2022-08-11 12:46:12 -0700

    bug in cropping of text on landscape vignettes

--- vignette.py
+++ vignette.py
@@ -8,8 +8,6 @@ from PIWordCloud import PI
 
 def makeVignette(thisPI):
 
-    title_short_side = 200
-
     path_to_wordcloud = 'results/220807/' + thisPI.lastName + '.png'
 
     if exists(path_to_wordcloud):
@@ -34,14 +32,12 @@ def makeVignette(thisPI):
             photo_height = math.ceil(vignette_height)
             photo_width = math.ceil(photo_height/photo_raw.height*photo_raw.width)
             title_long_side = img_wordcloud.height
-            vignette_width = img_wordcloud.width+photo_width+title_short_side
 
         else:
             vignette_width = img_wordcloud.width
             photo_width = math.ceil(vignette_width)
             photo_height = math.ceil(photo_width/photo_raw.width*photo_raw.height)
             title_long_side = img_wordcloud.width
-            vignette_height = img_wordcloud.height+photo_height+title_short_side    
 
         # ------ Resize
         photo = photo_raw.resize((photo_width, photo_height))
@@ -51,21 +47,16 @@ def makeVignette(thisPI):
         if vignette_is_landscape:
             vignette_height = img_wordcloud.height
             title_long_side = img_wordcloud.height
-            vignette_width = img_wordcloud.width+title_short_side
 
         else:
             vignette_width = img_wordcloud.width
             title_long_side = img_wordcloud.width
-            vignette_height = img_wordcloud.height+title_short_side    
-
-
-
 
     # ------- Create text
 
     title = thisPI.firstName.upper() + " " + thisPI.lastName.upper() + "@UCI"
 
-    img_title = Image.new("RGBA", (title_long_side,title_short_side), color=(255,255,255))
+    img_title = Image.new("RGBA", (title_long_side,400), color=(255,255,255))
     # Image is converted into editable form using
     # Draw function and assigned to d1
     img_draw = ImageDraw.Draw(img_title)
@@ -74,9 +65,26 @@ def makeVignette(thisPI):
     title_size = myFont.getsize(title)
     myFont = ImageFont.truetype("Arial.ttf", math.floor(title_long_side*200/title_size[0]))
 
+    title_short_side = myFont.getsize(title)[1]+10
+    print(title_short_side)
+    if photo_true == 1:
+        if vignette_is_landscape:
+            vignette_width = img_wordcloud.width+photo_width+title_short_side
+        else:
+            vignette_height = img_wordcloud.height+photo_height+title_short_side    
+    else:
+        if vignette_is_landscape:
+            vignette_width = img_wordcloud.width+title_short_side
+        else:
+            vignette_height = img_wordcloud.height+title_short_side    
+
     # Decide the text location, color and font
     #d1.text((65, 10), "Sample text", fill =(255, 0, 0),font=myFont)
     img_draw.text((title_long_side/2, 0), title, align="center",anchor='ma', fill =(0, 0, 0),font=myFont)
+    img_title.crop((0,0,title_long_side,title_short_side))
+
+    img_title.save(thisPI.lastName + '.png')
+ 
 
     if vignette_is_landscape:
         img_title = img_title.rotate(angle=-90,expand=True)
@@ -105,5 +113,5 @@ def makeVignette(thisPI):
 
 if __name__ == "__main__":
 
-    thisPI = PI("Emerson", "J")
+    thisPI = PI("Shi", "Xiaoyu")
     makeVignette(thisPI)
